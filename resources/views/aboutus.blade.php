286d924e1249d38ddbad209d54c6fba0097efc93
Farisy1998 <mca560@rajagiri.edu>
2021-03-17 15:36:41 +0530

    pushed

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,26 @@
+@extends("theme")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @section("content")
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <center><h2><u>About Us</u></h2></center>
+            <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium voluptatem vel maxime repudiandae obcaecati adipisci recusandae, quisquam velit assumenda, hic, aut suscipit temporibus expedita nulla ab at magnam quis in!
+            </div>
+            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptates similique, porro accusantium quod obcaecati dolorum facilis! Consequuntur voluptatibus qui repellat laudantium dolores, aliquid impedit voluptatum fugiat, natus et consectetur.
+            </div>
+        </div>
+    </div>
+    @endsection
+</body>
+</html>
